cab82a5027ce29eb62975666a0c1b37a3edc662d
andrescr00 <62442685+andrescr00@users.noreply.github.com>
2020-03-22 18:50:35 -0500

    Delete get.py

--- get.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://httpbin.org/get'
-args = {'nombre':'andres','apellido':'castano'}
-response = requests.get(url, params=args)
-
-if response.status_code == 200 :
-	print(response.text) 
